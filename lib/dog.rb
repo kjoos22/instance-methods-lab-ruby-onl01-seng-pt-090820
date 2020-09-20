1d2f7b87e61da17ff6771cb3e464289c52041775
Kristian Joos <kjoos22@gmail.com>
2020-09-20 17:29:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
+#Class to represent a dog
 class Dog
   
+  #method to allow Dog to .bark
+  def bark
+    puts "Woof!"
+  end
+  
+  #method to allow Dog to .sit
+  def sit
+    puts "The dog is sitting"
+  end
 end
