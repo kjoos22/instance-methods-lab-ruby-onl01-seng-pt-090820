b5dd8d2583c76e0e05baa9b82e9df04d1c9d4b63
Kristian Joos <kjoos22@gmail.com>
2020-09-20 17:31:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,13 @@
 #Class to represent a person
 def Person
   
-  #method to allow Person to talk
+  #method to allow Person to .talk
   def talk
+    puts "Hello World!"
+  end
+  
+  #method to allow Person to .walk
+  def walk
+    puts "The Person is walking"
+  end  
 end
